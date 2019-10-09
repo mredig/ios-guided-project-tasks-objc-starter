3583f931d0f263bfb92c0a37e4b8dab268bd8af2
Michael Redig <mredig@gmail.com>
2019-10-09 11:24:27 -0500

    organized

---
+++ Tasks/Tasks/LSITask.h
@@ -0,0 +1,17 @@
+//
+//  LSITask.h
+//  Tasks
+//
+//  Created by Michael Redig on 10/9/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSITask : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tasks/Tasks/LSITask.m
@@ -0,0 +1,13 @@
+//
+//  LSITask.m
+//  Tasks
+//
+//  Created by Michael Redig on 10/9/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import "LSITask.h"
+
+@implementation LSITask
+
+@end
